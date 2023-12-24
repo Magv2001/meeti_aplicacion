66f7fb91e89217506fbb3afc68391020da003e4b
Martin <74663759+Magv2001@users.noreply.github.com>
2023-12-24 13:33:00 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('meeti', 'postgres', 'martindatabase2001', {
+module.exports = new Sequelize('', '', '', {
     host: 'localhost',
     port: '5432',
     dialect: 'postgres',
