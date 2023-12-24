6daad3228c988363d4790df2984bf6dcd98fc0b1
Martin <74663759+Magv2001@users.noreply.github.com>
2023-12-24 13:31:32 -0500

    Update emails.js

--- config/emails.js
+++ config/emails.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: '667fe4e31f3eaf',
-    pass: '9128cb0dc10c07',
-    host: 'sandbox.smtp.mailtrap.io',
-    port: '2525'
+    user: '',
+    pass: '',
+    host: '',
+    port: ''
 }
